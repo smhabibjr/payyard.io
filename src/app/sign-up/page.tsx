c98e0e78687152021dfa163343dfbe3804a7fa65
Md Amanullah <amanullah1998bd@gmail.com>
2024-11-03 15:52:03 +0100

    Design signup page #3

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link'; // Import Link component
+import Link from 'next/link'; // Import Link from Next.js
 import { logo } from '../../../public/images';
 
-const LoginPage: React.FC = () => {
+const SignUpPage: React.FC = () => {
   return (
     <div className="flex flex-col w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 mx-auto p-8 md:p-10 2xl:p-12 3xl:p-14 bg-white rounded-2xl shadow-xl">
       <div className="flex items-center gap-3 pb-4 justify-center">
@@ -12,7 +12,7 @@ const LoginPage: React.FC = () => {
       </div>
 
       <div className="text-sm font-light text-[#3e62aa] pb-8 mx-auto">
-        Login to your account on FinTech.
+        Create a new account on FinTech.
       </div>
       <form className="flex flex-col" autoComplete="off">
         <div className="pb-2">
@@ -29,7 +29,8 @@ const LoginPage: React.FC = () => {
             <input type="email" name="email" id="email" className="pl-12 mb-2 bg-gray-50 text-gray-600 border border-gray-300 rounded-lg ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 w-full p-2.5 py-3 px-4" placeholder="name@company.com" />
           </div>
         </div>
-        <div className="pb-6">
+        
+        <div className="pb-2">
           <label htmlFor="password" className="block mb-2 text-sm font-medium text-[#111827]">
             Password
           </label>
@@ -45,21 +46,39 @@ const LoginPage: React.FC = () => {
             <input type="password" name="password" id="password" placeholder="••••••••••" className="pl-12 mb-2 bg-gray-50 text-gray-600 border border-gray-300 rounded-lg ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 w-full p-2.5 py-3 px-4" />
           </div>
         </div>
+
+        <div className="pb-6">
+          <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-[#111827]">
+            Confirm Password
+          </label>
+          <div className="relative text-gray-400">
+            <span className="absolute inset-y-0 left-0 flex items-center p-1 pl-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-asterisk">
+                <rect width="18" height="18" x="3" y="3" rx="2" />
+                <path d="M12 8v8" />
+                <path d="m8.5 14 7-4" />
+                <path d="m8.5 10 7 4" />
+              </svg>
+            </span>
+            <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••••" className="pl-12 mb-2 bg-gray-50 text-gray-600 border border-gray-300 rounded-lg ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 w-full p-2.5 py-3 px-4" />
+          </div>
+        </div>
+
         <button type="submit" className="w-full text-white bg-[#3e34fe] font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-6">
-          Login
+          Sign Up
         </button>
         <div className="text-sm font-light text-[#3e62aa] text-center">
-          Don&#39;t have an account yet?{' '}
-          <Link href="/sign-up" className="font-medium text-[#3e34fe] hover:underline">
-            Sign Up
-          </Link>
+          Already have an account?{' '}
+          <Link href="/sign-in" className="font-medium text-[#3e34fe] hover:underline">Login</Link>
         </div>
       </form>
+
       <div className="relative flex py-8 items-center">
         <div className="flex-grow border-t border-gray-200"></div>
         <span className="flex-shrink mx-4 font-medium text-gray-500">OR</span>
         <div className="flex-grow border-t border-gray-200"></div>
       </div>
+
       <form>
         <div className="flex flex-row gap-2 justify-center">
           <button className="flex flex-row w-32 gap-2 bg-gray-600 p-2 rounded-md text-gray-200">
@@ -81,4 +100,4 @@ const LoginPage: React.FC = () => {
   );
 };
 
-export default LoginPage;
+export default SignUpPage;
